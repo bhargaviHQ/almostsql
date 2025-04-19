1059a7b0b0473dca446beec6607ec33d9c0bc66a
Bhargavi Sriram <bhargavi1095@gmail.com>
2025-04-19 16:41:37 -0700

    Update learning_agent.py

--- agents/learning_agent.py
+++ agents/learning_agent.py
@@ -1,3 +1,5 @@
 class LearningAgent:
+    # This agent can also be though of as a NarratorAgent
+    # Purpose is to convey the learning output to the user
     def generate_learning_output(self, user_input, sql_query):
         return f"For your request: '{user_input}'\nGenerated SQL: {sql_query}"
